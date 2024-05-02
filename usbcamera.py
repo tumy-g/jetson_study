35651d536ecb37e1fe419abbf5a43c38b214bf18
tumy-g <e9085@g.maizuru-ct.ac.jp>
2024-05-02 13:08:05 +0900

    hahaha

--- usbcamera.py
+++ usbcamera.py
@@ -5,33 +5,33 @@ import mediapipe as mp
 mp_drawing = mp.solutions.drawing_utils
 mp_hands = mp.solutions.hands
 
-video_source = "/dev/video0"
-def gstreamer_pipeline(
-    capture_width=3264,
-    capture_height=2464,
-    display_width=3264,
-    display_height=2464,
-    framerate=21,
-    flip_method=0,
-):
-    return (
-        "nvarguscamerasrc ! "
-        "video/x-raw(memory:NVMM), "
-        "width=(int)%d, height=(int)%d, "
-        "format=(string)NV12, framerate=(fraction)%d/1 ! "
-        "nvvidconv flip-method=%d ! "
-        "video/x-raw, width=(int)%d, height=(int)%d, format=(string)BGRx ! "
-        "videoconvert ! "
-        "video/x-raw, format=(string)BGR ! appsink"
-        % (
-            capture_width,
-            capture_height,
-            framerate,
-            flip_method,
-            display_width,
-            display_height,
-        )
-    )
+video_source = "/dev/video1"
+# def gstreamer_pipeline(
+#     capture_width=3264,
+#     capture_height=2464,
+#     display_width=3264,
+#     display_height=2464,
+#     framerate=21,
+#     flip_method=0,
+# ):
+#     return (
+#         "nvarguscamerasrc ! "
+#         "video/x-raw(memory:NVMM), "
+#         "width=(int)%d, height=(int)%d, "
+#         "format=(string)NV12, framerate=(fraction)%d/1 ! "
+#         "nvvidconv flip-method=%d ! "
+#         "video/x-raw, width=(int)%d, height=(int)%d, format=(string)BGRx ! "
+#         "videoconvert ! "
+#         "video/x-raw, format=(string)BGR ! appsink"
+#         % (
+#             capture_width,
+#             capture_height,
+#             framerate,
+#             flip_method,
+#             display_width,
+#             display_height,
+#         )
+#     )
 
 
 # ウェブカメラのキャプチャを開始
