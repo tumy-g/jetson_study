23b471bad649bec3312007af5355fe7d019d58f0
tumy-g <e9085@g.maizuru-ct.ac.jp>
2024-05-02 11:17:53 +0900

    hahaha

---
+++ view_cameras.py
@@ -0,0 +1,9 @@
+#接続されているカメラのインデックスと解像度を表示するプログラム
+import cv2
+
+for i in range(10):
+    cap = cv2.VideoCapture(i)
+    if not cap.isOpened():
+        break
+    print(f"Camera {i}: {cap.get(cv2.CAP_PROP_FRAME_WIDTH)}x{cap.get(cv2.CAP_PROP_FRAME_HEIGHT)}")
+    cap.release()
