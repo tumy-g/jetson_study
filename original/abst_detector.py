c9262a6dcd808d5e139cf9a1a1aff46c6c399e4d
tumy-g <e9085@g.maizuru-ct.ac.jp>
2024-05-09 13:43:01 +0900

    add original/files

---
+++ original/abst_detector.py
@@ -0,0 +1,33 @@
+# -*- coding:utf-8 -*-
+#hand_detector.py で　import
+#クラスに「継承される前提」を付与することができる。継承せずにクラスを使用するとエラー
+from abc import ABCMeta, abstractmethod
+
+import numpy as np
+
+
+
+class AbstDetector(metaclass=ABCMeta):
+    @abstractmethod
+    def detect(self, image: np.ndarray) -> bool:
+        """モデルによる推論処理
+
+        Args:
+            image (np.ndarray): 入力イメージ
+
+        Returns:
+            bool: 対象物体（手や顔）が検出できたかどうか
+        """
+        pass
+
+    @abstractmethod
+    def draw(self, image: np.ndarray) -> np.ndarray:
+        """推論結果を描画する
+
+        Args:
+            image (np.ndarray): ベースイメージ
+
+        Returns:
+            np.ndarray: 描画済みイメージ
+        """
+        pass
---
+++ original/main.py
@@ -0,0 +1,43 @@
+from time import time, sleep
+import numpy as np
+import cv2
+import copy
+import mediapipe as mp
+
+#local files
+import hand_tracker
+
+#初期設定
+mp_drawing = mp.solutions.drawing_utils
+mp_hands = mp.solutions.hands
+
+cap = cv2.VideoCapture("/dev/video0")
+print("success capture")
+time.slep(1)
+
+#引数はとりあえず適当
+detector = hand_tracker.HandTracker(1, 0.5, 0.5)
+print("suceess handtrack")
+
+while cap.isOpened():
+    ret, image = cap.read()
+    if not ret:
+        print("Can't open camera")
+        break
+
+    tmp_image = copy.deepcopy(image)
+
+    if detector.detect(image):
+        tmp_image, tmp_landmark_dict = detector.draw(tmp_image)
+    
+    cv2.imshow('hand_tracker', cv2.resize(tmp_image, (480, 640)))
+
+    key = cv2.waitKey(1) & 0xFF
+    if key == ord('s'):
+        cv2.imwrite('../img_sacrifice/screenshot.png', tmp_image)
+    if key == ord('q'):
+        print("quit program")
+        break
+
+cap.release()
+cv2.destroyAllWindows()
