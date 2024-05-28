b6bb3f295a98b149da2a1289290d5e46d3e99093
tumy-g <e9085@g.maizuru-ct.ac.jp>
2024-05-28 15:40:02 +0900

    ランドマーク８の３次元座標を取得可能に

--- original/hand_tracker.py
+++ original/hand_tracker.py
@@ -83,11 +83,11 @@ class HandTracker(AbstDetector):
                         continue
                     cv2.line(image, u, v, landmark_color["stress"], 8)
                     if con_pair in [(5,6)]:
+                        print("人差し指：",u,v)
                         print("x:",hand_landmarks.landmark[8].x * base_width)#landmark0(手首)の3次元座標を表示
                         print("y:",hand_landmarks.landmark[8].y * base_height)#landmark0(手首)の3次元座標を表示
-                        print("z:",hand_landmarks.landmark[8].z * 100)#landmark0(手首)の3次元座標を表示
+                        print("z:",hand_landmarks.landmark[8].z * 100, end="\n")#landmark0(手首)の3次元座標を表示
 
-                        print("人差し指：",u,v)
                     # elif con_pair in [(9,10)]:
                     #     print("中指：", u,v)
                     # elif con_pair in [(17,18)]:
