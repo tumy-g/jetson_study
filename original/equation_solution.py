8d0ca650127dadd48a107ff3ac1acc8c37dfe648
tumy-g <e9085@g.maizuru-ct.ac.jp>
2024-06-20 13:11:20 +0900

    次は方程式に当てはめる

---
+++ original/equation_solution.py
@@ -0,0 +1,11 @@
+# -*- coding:utf-8
+
+def equation(theta1,theta2,theta3):
+    '''
+    方程式[y = 1.457 - 0.001138a - 0.001883b - 0.002875c]の結果をリターンする
+    Args:
+        theta1: 
+        theta2: 
+        theta3: 
+    '''
+    pass
