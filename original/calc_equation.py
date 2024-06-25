d2f09f87b1f583b12c65060ced935f519da0c1a8
tumy-g <e9085@g.maizuru-ct.ac.jp>
2024-06-25 14:13:25 +0900

    リアルタイムでLasso回帰直線式の結果が描画されるように

--- original/calc_equation.py
+++ original/calc_equation.py
@@ -5,5 +5,4 @@ def formula(angle1,angle2,angle3)->float:
     3つのなす角をLassoの回帰直線式にあてはめ、その結果をリターンする
     '''
     y = 1.457 - 0.001138*angle1 - 0.001883*angle2 - 0.002875*angle3
-    print(y)
     return y
