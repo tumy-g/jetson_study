7f6653ca98f3cee25c9f953cde5eeeb9da168614
tumy-g <e9085@g.maizuru-ct.ac.jp>
2024-07-23 15:49:30 +0900

    ほげ

--- original/draws.py
+++ original/draws.py
@@ -17,11 +17,11 @@ def draw_result(image, x1, y1, x2, y2)->None:
     cv2.putText(image, "Calc:", pt, font, font_scale, black, thickness=1)
     cv2.rectangle(image, pt1b, pt2b, green, thickness)
 
-def draw_finger_angle(image, name:str, x1, y1, x2, y2)->None:
+def draw_finger_angle(image, name:str,angle, x1, y1, x2, y2)->None:
     pt = (x1, y1)
 
     pt1b = (x1, y1+10)
     pt2b = (x2, y2+10)
     
-    cv2.putText(image, name, pt, font, font_scale, black, thickness=1)
+    cv2.putText(image, name + ":" + str(angle)[0:5], pt, font, font_scale, black, thickness=1)
     cv2.rectangle(image, pt1b, pt2b, green, thickness)
