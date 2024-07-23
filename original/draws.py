b3f25823d1bc52ae6bfee8246e4f123d6d1c2a7f
tumy-g <e9085@g.maizuru-ct.ac.jp>
2024-07-23 14:40:04 +0900

    ほげ

--- original/draws.py
+++ original/draws.py
@@ -2,8 +2,11 @@ import cv2
 import numpy as np
 
 def draw_result(image, x1, y1, x2, y2):
-    pt1 = (x1, y1)
-    pt2 = (x2, y2)
+    pt1a = (x1, y1)
+    pt2a = (x2, y2)
+
+    pt1b = (x1 + 50, y1)
+    pt2b = (x2 + 50, y2)
 
     green = (0,255,0)
     thickness = 2
@@ -12,5 +15,5 @@ def draw_result(image, x1, y1, x2, y2):
     black = (0,0,0)
     thickness = 1
 
-    cv2.putText(image, "計算結果:", pt1-(20,20), font, font_scale, black, thickness)
-    cv2.rectangle(image, pt1, pt2, green, thickness)
+    cv2.putText(image, "計算結果:", pt1a, font, font_scale, black, thickness)
+    cv2.rectangle(image, pt1b, pt2b, green, thickness)
