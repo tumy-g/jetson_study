44846a8fd05cee03c312ae894da8eb6c14a8600e
tumy-g <e9085@g.maizuru-ct.ac.jp>
2024-07-23 14:35:57 +0900

    ほげ

--- original/draws.py
+++ original/draws.py
@@ -1,10 +1,16 @@
 import cv2
 import numpy as np
 
-def draw_rectangle(image, x1, y1, x2, y2):
+def draw_result(image, x1, y1, x2, y2):
     pt1 = (x1, y1)
     pt2 = (x2, y2)
 
-    color = (0,255,0)
+    green = (0,255,0)
     thickness = 2
-    cv2.rectangle(image, pt1, pt2, color, thickness)
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    font_scale = 0.8
+    black = (0,0,0)
+    thickness = 1
+
+    cv2.putText(image, "計算結果:", pt1-(20,20), font, font_scale, black, thickness)
+    cv2.rectangle(image, pt1, pt2, green, thickness)
